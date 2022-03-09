3aaf306ab03d5249cccd7cffeb1395d9655a3731
Perruchock <diego_antonio_17@live.com.mx>
2022-03-08 18:05:19 -0600

    ahi va esto

--- phoname.py
+++ phoname.py
@@ -1,4 +1,26 @@
-print("Ingresa un número de teléfono:  ")
-number = input()
-print (number)
+# Get string 
+print("Ingresa un número:  ")
+number = str(input())
+
+#Convert string to number as specified.
+
+# Print number 
+for n in range(8): # File texts have only 8 lines
+    linetoprint = ""
+    # Built the nth line:
+    for i in number:
+        try:
+            f = open(f'{i}.txt', 'r')
+        except FileNotFoundError:
+            print("Entrada no válida")
+            exit()
+        lines = f.readlines()
+        linetoprint = linetoprint + lines[n][:-1] #There must be a "/n" as the last char of the string, so remove it. 
+        f.close()
+    print(linetoprint)
+    
+
+
+
+
 
