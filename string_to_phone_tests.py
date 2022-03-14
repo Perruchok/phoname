6ff1d48e80eff64c53c5d76686f6ccd31b26b90a
Perruchock <diego_antonio_17@live.com.mx>
2022-03-14 13:12:28 -0600

    started modifications on unit testing

--- string_to_phone_tests.py
+++ string_to_phone_tests.py
@@ -1,12 +1,25 @@
 # Import the unittest library and our function
 import unittest
-from helpers import convert
+from helpers import convert, symbol_relations
 
 # Test convert function 
 class TestConvert(unittest.TestCase):
 
-    def test_1(self):
-        self.assertTrue(convert("((Love)) + -You") == "aa5683bbd+dc968")
+    MAPPER = {
+        "(": "open_parenthesis",
+        ")": "close_parenthesis",
+        "-": "minus", 
+        " ": "space",
+    }
+
+    def _get_expected_output(self,number):
+        return [
+            f"" f"{TestConvert.MAPPER[symbol]}.txt" for symbol in number
+            ]
+
+    def test_convert(self):
+        expected_output = self._get_expected_output('((5683)) + -968')
+        self.assertTrue(convert("((Love)) + -You") == expected_output)
 
 # Run each of the testing functions
 if __name__ == "__main__":
